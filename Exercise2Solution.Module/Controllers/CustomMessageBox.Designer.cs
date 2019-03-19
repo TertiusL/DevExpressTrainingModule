63f3475b729a0403a8f8ef53112d75dc1d45d084
Tertius <liebenberg.tertius@gmail.com>
2019-03-19 11:26:23 +0200

    trying to fix logo nonsense

---
+++ Exercise2Solution.Module/Controllers/CustomMessageBox.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Exercise2Solution.Module.Controllers
+{
+    partial class CustomMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CustomMessageAction = new DevExpress.ExpressApp.Actions.PopupWindowShowAction(this.components);
+            // 
+            // CustomMessageAction
+            // 
+            this.CustomMessageAction.AcceptButtonCaption = null;
+            this.CustomMessageAction.CancelButtonCaption = null;
+            this.CustomMessageAction.Caption = "Custom Message Action";
+            this.CustomMessageAction.ConfirmationMessage = null;
+            this.CustomMessageAction.Id = "CustomMessageAction";
+            this.CustomMessageAction.ToolTip = null;
+            this.CustomMessageAction.Execute += new DevExpress.ExpressApp.Actions.PopupWindowShowActionExecuteEventHandler(this.CustomMessageAction_Execute);
+            // 
+            // CustomMessageBox
+            // 
+            this.Actions.Add(this.CustomMessageAction);
+            this.TargetObjectType = typeof(Exercise2Solution.Module.BusinessObjects.Orders);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.PopupWindowShowAction CustomMessageAction;
+    }
+}
---
+++ Exercise2Solution.Module/Controllers/CustomMessageBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using Exercise2Solution.Module.BusinessObjects;
+
+namespace Exercise2Solution.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class CustomMessageBox : ViewController
+    {
+        public CustomMessageBox()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void CustomMessageAction_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            
+        }
+    }
+}
